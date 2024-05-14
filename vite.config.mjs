894db86e1ef07426fe6a4d2b97b219527f2e0214
ultraza4 <aza_kz14@mail.ru>
2024-05-15 02:09:57 +0500

    fix: change base for gh_pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,6 +10,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/binance_depth/",
   plugins: [
     Vue({
       template: { transformAssetUrls }
