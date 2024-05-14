e7ae407901cdaa404ea1ebe83fdb49852a124353
ultraza4 <aza_kz14@mail.ru>
2024-05-15 02:47:50 +0500

    fix: user simple path in router

--- src/router/router.js
+++ src/router/router.js
@@ -2,8 +2,8 @@ import { createWebHashHistory, createRouter } from 'vue-router'
 
 const routes = [
   { path: '/', redirect: '/Settings'},
-  { path: '/Settings', component: import('@/pages/SettingsPage.vue') },
-  { path: '/OrderBook', component: import('@/pages/OrderBookPage.vue') },
+  { path: '/Settings', component: import('../pages/SettingsPage.vue') },
+  { path: '/OrderBook', component: import('../pages/OrderBookPage.vue') },
 ]
 
 const router = createRouter({
